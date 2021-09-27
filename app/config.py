78287d8d2b1abb37a882c0229188f588e2da9905
askvart <tur1amant@gmail.com>
2021-09-27 21:12:38 +0300

    class Configuration

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Configuration(object):
     DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://gvg:passw1@localhost/gvg_db2'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
